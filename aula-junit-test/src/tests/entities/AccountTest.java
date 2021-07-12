e932ad8a0d8d302f5f1429d19b88ecba67cde4af
Luiz Moraes <lffm1994@gmail.com>
2021-07-11 23:31:26 -0300

    02-09 Teste deposito deveria fazer nada quando quantia for negativa

--- aula-junit-test/src/tests/entities/AccountTest.java
+++ aula-junit-test/src/tests/entities/AccountTest.java
@@ -8,21 +8,45 @@ import org.junit.jupiter.api.Test;
 import entities.Account;
 
 class AccountTest {
+	
+	/*
+	 * Função Deposito deve incrementar e descontar a taxa quando a quantia for positiva
+	 */
 
 	@Test
 	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
-		//Arrange cenario
+		// Arrange cenario
 		double amount = 200.0;
 		double expectedValue = 196.0;
 		Account acc = new Account(1L, 0.0);
+
+		// Act acao
+		acc.deposit(amount);
+
+		// Assert verificacao se os valores são iguais
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+
+	}
+	/*
+	 * Função deposito não faz nada quando informar uma quantia negatia
+	 */
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		// Arrange cenário
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, 0.0);
+		double amount = -200;
 		
-		//Act acao
+		// Act ação
 		acc.deposit(amount);
 		
-		//Assert verificacao se os valores são iguais
+		// Assert verificação
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 		
-		
+	
+	
 	}
+	
+	
 
 }
